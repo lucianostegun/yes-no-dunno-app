ef5a2f9d6899e1384fe40a1a65af75b4ff1f3d62
Luciano Stegun <lucianostegun@gmail.com>
2024-02-06 13:54:04 -0300

    Add button click bindings

--- Yes No Dunno/ViewController.swift
+++ Yes No Dunno/ViewController.swift
@@ -13,22 +13,88 @@ class ViewController: UIViewController {
     @IBOutlet weak var yesButton: UIButton!
     @IBOutlet weak var noButton: UIButton!
     @IBOutlet weak var dunnoButton: UIButton!
-    @IBOutlet weak var configModeSwtich: UISwitch!
-    
-    @IBOutlet weak var recordingLabel: UILabel!;
-    @IBOutlet weak var recordButton: UIButton!
-    @IBOutlet weak var saveButton: UIButton!
-    @IBOutlet weak var cancelButton: UIButton!
+    @IBOutlet weak var configModeSwitch: UISwitch!
     
     var recorder: AVAudioRecorder?
     var player: AVAudioPlayer?
     var recordTag: String = ""
     var settings: [String: Any]?
-        
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.setupRecorder()
+        self.setupButtons()
     }
+    
+    @IBAction func onClickYesButton(sender: UIButton) {
+        recordTag = "yes";
+        if (configModeSwitch.isOn) {
+            sender.setTitle("GRAVANDO", for: .normal)
+            self.record()
+        } else {
+            self.play()
+        }
+    }
+    
+    @IBAction func onClickNoButton(sender: UIButton) {
+        recordTag = "no";
+        if (configModeSwitch.isOn) {
+            sender.setTitle("GRAVANDO", for: .normal)
+            self.record()
+        } else {
+            self.play()
+        }
+    }
+    
+    @IBAction func onClickDunnoButton(sender: UIButton) {
+        recordTag = "dunno";
+        if (configModeSwitch.isOn) {
+            sender.setTitle("GRAVANDO", for: .normal)
+            self.record()
+        } else {
+            self.play()
+        }
+    }
+    
+    @IBAction func onTouchUpButton(sender: UIButton) {
+        if (configModeSwitch.isOn) {
+            print("Stop recording");
+            recorder?.stop()
+            self.play()
+            
+            switch (recordTag) {
+            case "yes":
+                yesButton.setTitle("SIM", for: .normal)
+                break
+            case "no":
+                noButton.setTitle("NÃO", for: .normal)
+                break
+            case "dunno":
+                dunnoButton.setTitle("NÃO SEI", for: .normal)
+                break
+            default:
+                break
+            }
+        }
+    }
+    
+    func setupButtons() {
+            // Set corner radius for yesButton
+            yesButton.layer.cornerRadius = 20 // Adjust the value as needed
+            yesButton.clipsToBounds = true
+            yesButton.titleLabel?.font = UIFont.boldSystemFont(ofSize: 22) // Adjust the size as needed
+
+
+            // Set corner radius for noButton
+            noButton.layer.cornerRadius = 20 // Adjust the value as needed
+            noButton.clipsToBounds = true
+            noButton.titleLabel?.font = UIFont.boldSystemFont(ofSize: 26) // Adjust the size as needed
+
+            // Set corner radius for dunnoButton
+            dunnoButton.layer.cornerRadius = 20 // Adjust the value as needed
+            dunnoButton.clipsToBounds = true
+            dunnoButton.titleLabel?.font = UIFont.boldSystemFont(ofSize: 30) // Adjust the size as needed
+        }
     
     func setupRecorder() {
         print("setupRecorder")
@@ -80,7 +146,7 @@ class ViewController: UIViewController {
         // Set up and play the recorded audio
         print("Playing for " + recordTag)
         do {
-//            try AVAudioSession.sharedInstance().setCategory(.playAndRecord, mode: .default, options: [.defaultToSpeaker])
+            try AVAudioSession.sharedInstance().setCategory(.playAndRecord, mode: .default, options: [.defaultToSpeaker])
         } catch {
             print("Error setting audio session category: \(error.localizedDescription)")
         }
